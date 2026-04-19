90ee60aeab54e6f5d4d2b3ddceba28f0172b28c8
Vladislav Elizarov <velizarovdev@gmail.com>
2026-04-19 15:24:45 +0300

    Update index styles path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/fonts.css'
-import './index.css'
+import './styles/index.css'
 import App from './components/App'
 
 createRoot(document.getElementById('root')).render(
